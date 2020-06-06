101037e02afa92d4fc7d86c93cbd7990a012772a
PlayFriik <playfriik@gmail.com>
2020-06-06 17:41:35 +0100

    op3: Change build fingerprint to Pixel 4 XL (June 2020)
    * Using the Oreo fingerprint passes the SafetyNet, but it does not let us use ARCore

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -45,13 +45,13 @@ constexpr const char* RO_PROP_SOURCES[] = {
 };
 
 constexpr const char* BUILD_DESCRIPTION[] = {
-        "OnePlus3-user 8.0.0 OPR1.170623.032 31 release-keys",
-        "OnePlus3-user 8.0.0 OPR1.170623.032 31 release-keys",
+        "coral-user 10 QQ3A.200605.001 6392402 release-keys",
+        "coral-user 10 QQ3A.200605.001 6392402 release-keys",
 };
 
 constexpr const char* BUILD_FINGERPRINT[] = {
-        "OnePlus/OnePlus3/OnePlus3:8.0.0/OPR1.170623.032/02281230:user/release-keys",
-        "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR1.170623.032/02281230:user/release-keys",
+        "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys",
+        "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys",
 };
 
 void property_override(char const prop[], char const value[]) {
