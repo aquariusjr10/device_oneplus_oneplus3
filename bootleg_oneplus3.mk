0cbac94681b70d856ce615864f031409fca2c731
Deepak <deepakjeganathan@gmail.com>
2020-06-07 16:05:57 +0200

    Adapt to Bootleggers

--- bootleg_oneplus3.mk
+++ bootleg_oneplus3.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
-PRODUCT_NAME := lineage_oneplus3
+PRODUCT_NAME := bootleg_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -36,3 +36,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 TARGET_VENDOR := oneplus
+#Bootleg stuffes
+DEVICE_MAINTAINERS := Deepakjr
+BOOTLEGGERS_BUILD_TYPE := Shishufied
+#Changelog fix
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
