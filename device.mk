c13381ab9c1987f20f93d14f278f54cb96d423cb
deepakjr <deepakjeganathan@gmail.com>
2020-07-14 15:22:48 +0200

    Inherit from oneplus settings

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
+$(call inherit-product, device/oneplus/settings/common.mk
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
 # Doze mode
 PRODUCT_PACKAGES += \
     CustomDoze
-		
+
 # Spectrum
 PRODUCT_PACKAGES += \
    init.spectrum.rc \
